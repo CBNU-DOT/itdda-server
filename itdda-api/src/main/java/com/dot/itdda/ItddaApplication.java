45a8453189ca1a1fc7c4ecf569d0bf9b2214c573
minseok-oh <minseok.dev@gmail.com>
2024-02-25 23:10:02 +0900

    setting: spring boot 기본 앱 빌드

---
+++ itdda-api/src/main/java/com/dot/itdda/ItddaApplication.java
@@ -0,0 +1,13 @@
+package com.dot.itdda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItddaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItddaApplication.class, args);
+	}
+
+}
