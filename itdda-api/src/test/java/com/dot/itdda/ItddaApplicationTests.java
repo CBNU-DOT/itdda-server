e6fc502920c33e3f4aec418f4b2a4e0f34addad5
MinSeok Oh <minseok.dev@gmail.com>
2024-02-26 11:55:05 +0900

    setting/initial-setting -> develop (#2)
    * setting: spring boot 기본 앱 빌드
    
    * setting: spring boot dockerfile 생성
    
    * setting: github issue, pr template 생성
    
    * feat: docker compose api, db 추가
    
    * chore: .env 파일 .gitignore에 추가

---
+++ itdda-api/src/test/java/com/dot/itdda/ItddaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dot.itdda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItddaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
